068d5784d62da70fccd1f94f31a04101d0cb9de5
azmisal <b20cs33@gectcr.ac.in>
2024-12-11 00:28:14 +0530

    check 7

--- api/routes/items.js
+++ api/routes/items.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const userCopy = require('../model/user');
 
 // GET request to fetch all items
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
+    const strings = req.body
     try {
-        const users = await userCopy.find(); // Fetch all documents in the collection
-        res.status(200).json(users); // Send the data as JSON
+        // const users = await userCopy.find(); // Fetch all documents in the collection
+        res.send(strings); // Send the data as JSON
     } catch (error) {
         console.error("Error fetching data:", error);
         res.status(500).json({ message: "Failed to fetch data", error });
