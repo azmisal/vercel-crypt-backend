e465216bdfefc284581d73be07ece18e9309dc3d
azmisal <b20cs33@gectcr.ac.in>
2024-12-11 00:08:07 +0530

    check 6

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const cors = require("cors");
+const cors = require("cors");
 
 require("dotenv").config();
 const app = express();
 
 // Enable CORS for http://localhost:3000
-// app.use(cors());
+app.use(cors());
 
 
 // Middleware to parse JSON
